f88747bbe6a90dd1336640bed0262fcd6ca0ef17
Cristian M D Blanco <crismablanco@gmail.com>
2016-02-29 11:38:27 -0300

    README and PEP8
    - ADDED A README FILE
    - THE TOURNAMENT.PY WAS FOMATTED WITH PEP8

---
+++ vagrant/tournament/README.md
@@ -0,0 +1,84 @@
+# Tournament Planner Project
+Project Description:
+In this project, you’ll be see a Python module that uses the PostgreSQL
+database to keep track of players and matches in a game tournament.
+
+The game tournament will use the Swiss system for pairing up players in each
+round: players are not eliminated, and each player should be paired with
+another player with the same number of wins, or as close as possible.
+
+This project has two parts: 
+    - `tournament.sql` the database schema (SQL table definitions),
+	- `tournament.py`  the code that will use it.
+This is a Python and Relational Data Base project where you can handle a
+tournament with the Swiss System
+
+
+## Introduction
+This is a project made for Udacity Nanodegree Program of Full Stack Web Developer. 
+You can see it 
+[here](https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd004)
+It can create a Tournament, Register Players, Register Matches between players,
+	create the next round pairs according to their scores and more.
+It is the final project of the
+[Intro to Relational Databases course](https://www.udacity.com/courses/ud197)
+
+
+## Requierments
+The code has been created using Python 2.7.
+It is running inside a Vagrant Machine.
+Just run the `tournament_test.py` and it will run a full test of the tournament.
+If you edit the file you will be able to create your own tournaments.
+
+## Iside the code
+In this project I created a `tournament.py` and a `tournament.sql` that 
+	stores the code of the tournament generation and the database:
+
+### tournament.sql:
+	Be careful, ths system will drop a tournament existing database
+`DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+CREATE TABLE players (...);
+CREATE TABLE tournaments (...);
+CREATE TABLE tournaments_players (...);
+CREATE TABLE matches (...);`
+
+### tournament.py
+
+	Here are the function inside the file:
+
+- `def connect()`: Connect to the PostgreSQL database.  Returns a database connection.
+- `def deleteMatches()`: Remove all the match records from the database.
+- `def deletePlayers()`: Clear out all the player records from the database.
+- `def deleteTournaments()`: Remove all the tournaments records from the database.
+- `def registerTournament(name)`: Adds a tournament to tournament database.
+The function returns the ID of the tournament added. The database assigns a
+unique serial id number for the tournament.
+- `def countPlayers(tournament_id)`: Returns the number of players currently
+registered on an specific tournament.
+- `def registerPlayer(name, tournament_id)`: Adds a player to the tournament by
+putting an entry in the database. The database should assign an ID number to the
+player. Different players may have the same names but will receive different 
+ID numbers.
+- `def asociatePlayerIntoTournament(player_id, tournament_id)`: Adds a player
+into a tournament. The player is already stored on database.
+- `def playerStandings(tournament_id)`: Returns a list of the players and their
+win records, sorted by wins. The first entry in the list should be the player in
+first place, or a player tied for first place if there is currently a tie.
+- `def reportMatch(tournament_id, winner, loser, draw)`: Records the outcome of
+a single match between two players.
+- `def doBye(tournament_id, player_id)`: Generate a bye on the tournament
+- `def hasBye(tournament_id, player_id)`: check if a player has been already 'bye'
+- `def alreadyPlay(tournament_id, player_id1, player_id2)`: Prevent rematches between players
+- `def swissPairings(tournament_id)`: Returns a list of pairs of players for the
+next round of a match. Assuming that there are an even number of players
+registered, each player appears exactly once in the pairings.  Each player is
+paired with another player with an equal or nearly-equal win record, that is,
+a player adjacent to him or her in the standings.
+
+
+## Contact
+For contact send an email to crismablanco@gmail.com
+
+Best Regards
+Cristian Blanco
--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
+
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
@@ -18,7 +19,7 @@ def deleteMatches():
     c.execute(query)
     DB.commit()
     DB.close
-    
+
 
 def deletePlayers():
     """Remove all the player records from the database."""
@@ -41,10 +42,10 @@ def deleteTournaments():
 
 
 def registerTournament(name):
-    """Adds a tournament to tournament database. the function returns the ID of the tournament added
-  
+    """Adds a tournament to tournament database.
+        The function returns the ID of the tournament added
     The database assigns a unique serial id number for the tournament.
-  
+
     Args:
       name: the tournament's name (need not be unique).
     """
@@ -56,23 +57,27 @@ def registerTournament(name):
     DB.commit()
     DB.close()
     return lastTournamentAdded
-    
+
+
 def countPlayers(tournament_id):
-    """Returns the number of players currently registered on an specific tournament."""
+    """Returns the number of players currently registered
+        on an specific tournament."""
     DB = connect()
     c = DB.cursor()
-    query = "SELECT count(player_id) as cp FROM tournaments_players WHERE tournament_id = %s"
-    c.execute(query,(tournament_id,))
+    query = """SELECT count(player_id) as cp
+                FROM tournaments_players
+                WHERE tournament_id = %s"""
+    c.execute(query, (tournament_id,))
     countP = c.fetchone()[0]
     DB.close()
     return countP
 
+
 def registerPlayer(name, tournament_id):
     """Adds a player into a tournament to tournament database.
-  
-    The database assigns a unique serial id number for the player.  (This
+     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
       tournament_id: the tournament's ID where to register the player
@@ -84,11 +89,13 @@ def registerPlayer(name, tournament_id):
     lastPlayerAdded = c.fetchone()[0]
     DB.commit()
     DB.close()
-    asociatePlayerIntoTournament(lastPlayerAdded,tournament_id)
+    asociatePlayerIntoTournament(lastPlayerAdded, tournament_id)
 
-def asociatePlayerIntoTournament(player_id,tournament_id):
-    """ 
-        Adds a player into a tournament. The player is already stored on database.
+
+def asociatePlayerIntoTournament(player_id, tournament_id):
+    """
+        Adds a player into a tournament.
+        The player is already stored on database.
 
         Args:
             player_id: the id of the player to asociate.
@@ -98,35 +105,36 @@ def asociatePlayerIntoTournament(player_id,tournament_id):
     DB = connect()
     c = DB.cursor()
     query = "SELECT * FROM players WHERE id = %s"
-    c.execute(query,(player_id,))
+    c.execute(query, (player_id,))
     exist = c.fetchall()
-    if exist==[]:
+    if exist == []:
         raise ValueError("The player does not exist on database.")
-    
     query = "SELECT * FROM tournaments WHERE id = %s"
-    c.execute(query,(tournament_id,))
+    c.execute(query, (tournament_id,))
     exist = c.fetchall()
-    if exist==[]:
+    if exist == []:
         raise ValueError("The tournament does not exist on database.")
 
-    query = "SELECT * FROM tournaments_players WHERE tournament_id = %s AND player_id = %s"
-    c.execute(query,(tournament_id, player_id,))
+    query = """SELECT * FROM tournaments_players
+                WHERE tournament_id = %s AND player_id = %s"""
+    c.execute(query, (tournament_id, player_id,))
     exist = c.fetchall()
-    if exist==[]:
-        query = "INSERT INTO tournaments_players (tournament_id, player_id) VALUES (%s, %s)"
-        c.execute(query, (tournament_id,player_id))
+    if exist == []:
+        query = """INSERT INTO tournaments_players
+                (tournament_id, player_id) VALUES (%s, %s)"""
+        c.execute(query, (tournament_id, player_id))
         DB.commit()
         print "The player was successfully asociated to the tournament"
     else:
         print "The player is already asociated to the tournament"
     DB.close()
-    
+
 
 def playerStandings(tournament_id):
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place,
+    or a player tied for first place if there is currently a tie.
 
     Args:
       tournament_id: the tournament ID for the standings.
@@ -140,29 +148,33 @@ def playerStandings(tournament_id):
     """
     DB = connect()
     c = DB.cursor()
-    query = """ SELECT  p.id, 
-                        p.name, 
-                        (SELECT COUNT(*) FROM matches m WHERE m.winner_id = p.id AND m.draw = False AND m.tournament_id = %s) AS wins,
-                        (SELECT COUNT(*) FROM matches m WHERE m.tournament_id = %s AND (m.winner_id = p.id OR m.loser_id = p.id)) AS matches,
+    query = """ SELECT  p.id,
+                        p.name,
+                        (SELECT COUNT(*) FROM matches m
+                            WHERE m.winner_id = p.id
+                                AND m.draw = False
+                                AND m.tournament_id = %s) AS wins,
+                        (SELECT COUNT(*) FROM matches m
+                            WHERE m.tournament_id = %s
+                                AND (m.winner_id = p.id OR m.loser_id = p.id))
+                                    AS matches,
                         (SELECT COUNT(*) FROM matches momw
-                            WHERE momw.draw = False 
-                            AND (momw.winner_id IN (SELECT loser_id
-                                                        FROM matches m
-                                                        WHERE m.winner_id = p.id and m.bye = 0 
-                                                        AND m.tournament_id = %s)
-                            OR momw.winner_id IN (SELECT winner_id
-                                                        FROM matches m
-                                                        WHERE m.loser_id = p.id and m.bye = 0
-                                                        AND m.tournament_id = %s))) AS omw
-                        FROM players p LEFT JOIN tournaments_players tp on (p.id = tp.player_id)
+                            WHERE momw.draw = False
+                            AND (momw.winner_id IN
+                                (SELECT loser_id
+                                    FROM matches m
+                                    WHERE m.winner_id = p.id and m.bye = 0
+                                    AND m.tournament_id = %s)
+                            OR momw.winner_id IN
+                                (SELECT winner_id
+                                    FROM matches m
+                                    WHERE m.loser_id = p.id
+                                        AND m.bye = 0
+                                        AND m.tournament_id = %s))) AS omw
+                        FROM players p LEFT JOIN tournaments_players tp
+                            on (p.id = tp.player_id)
                         WHERE tp.tournament_id = %s
-                        ORDER BY wins DESC, omw ASC
-                        
-            """
-
-
-
-
+                        ORDER BY wins DESC, omw ASC"""
 
     c.execute(query, (tournament_id, tournament_id, tournament_id, tournament_id, tournament_id,))
     standings = []
@@ -181,15 +193,16 @@ def reportMatch(tournament_id, winner, loser, draw):
       loser:  the id number of the player who lost
       draw: true or false as appropiate
     """
-    
     DB = connect()
     c = DB.cursor()
-    query = "INSERT INTO matches (tournament_id, winner_id, loser_id, draw) VALUES (%s,%s,%s,%s)"
+    query = """INSERT INTO matches (tournament_id, winner_id, loser_id, draw)
+                VALUES (%s,%s,%s,%s)"""
     c.execute(query, (tournament_id, winner, loser, draw))
     DB.commit()
     DB.close()
 
-def doBye(tournament_id,player_id):
+
+def doBye(tournament_id, player_id):
     """ Generate a bye on the tournament
 
     Args:
@@ -197,20 +210,17 @@ def doBye(tournament_id,player_id):
     """
     DB = connect()
     c = DB.cursor()
-    
-    query = "INSERT INTO matches (tournament_id, winner_id, loser_id, draw, bye) VALUES (%s,%s,%s,%s,%s)"
-    
-    c.execute(query,(tournament_id,player_id,player_id,False,1,))
+    query = """INSERT INTO matches (tournament_id, winner_id, loser_id, draw, bye)
+                VALUES (%s,%s,%s,%s,%s)"""
+    c.execute(query, (tournament_id, player_id, player_id, False, 1,))
     DB.commit()
-    
     DB.close()
     return True
 
 
-
-def hasBye(tournament_id,player_id):
-    """ 
-    -- THIS IS OPTIONAL -- 
+def hasBye(tournament_id, player_id):
+    """
+    -- THIS IS OPTIONAL --
 
     check if a player has been already 'bye'
 
@@ -225,18 +235,19 @@ def hasBye(tournament_id,player_id):
 
     DB = connect()
     c = DB.cursor()
-    query = "SELECT bye FROM matches WHERE tournament_id = %s and (winner_id = %s or loser_id = %s)"
-    c.execute(query,(tournament_id,player_id,player_id,))
+    query = """SELECT bye FROM matches
+                WHERE tournament_id = %s
+                    AND (winner_id = %s OR loser_id = %s)"""
+    c.execute(query, (tournament_id, player_id, player_id,))
     playerBye = c.fetchone()[0]
     DB.close()
-    if playerBye==0:
-        #print "Player hasn't got a bye"
+    if playerBye == 0:
         return False
     else:
         return True
 
 
-def alreadyPlay(tournament_id,player_id1,player_id2):
+def alreadyPlay(tournament_id, player_id1, player_id2):
     """Prevent rematches between players
 
     Args:
@@ -250,23 +261,27 @@ def alreadyPlay(tournament_id,player_id1,player_id2):
     """
     DB = connect()
     c = DB.cursor()
-    query = "SELECT id FROM matches WHERE tournament_id = %s and ((winner_id = %s and loser_id = %s) or (winner_id = %s and loser_id = %s))"
-    c.execute(query,(tournament_id,player_id1,player_id2,player_id2,player_id1,))
+    query = """SELECT id FROM matches
+                WHERE tournament_id = %s
+                    AND ((winner_id = %s and loser_id = %s)
+                        OR (winner_id = %s and loser_id = %s))"""
+    c.execute(query, (tournament_id, player_id1, player_id2, player_id2, player_id1,))
     theyPlayed = c.fetchall()
     DB.close()
-    if theyPlayed!=[]:
+    if theyPlayed != []:
         return True
     else:
         return False
 
+
 def swissPairings(tournament_id):
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-    
+
     Args:
       tournament_id: the ID of the current tournament
 
@@ -277,38 +292,33 @@ def swissPairings(tournament_id):
         id2: the second player's unique id
         name2: the second player's name
     """
-
     results = playerStandings(tournament_id)
-    pairs = [] #this will be the list of tuples, each of which contains (id1, name1, id2, name2)
-
+    pairs = []
     numberOfPlayers = countPlayers(tournament_id)
-    #print "number of players: "+str(numberOfPlayers)
 
     """ Here is the BYE zone """
-    if numberOfPlayers %2 != 0: 
+    if numberOfPlayers % 2 != 0:
         i = 0
         byeDone = False
         while not byeDone:
-            if results[i][5]==0:
-                doBye(tournament_id,results[i][0])
+            if results[i][5] == 0:
+                doBye(tournament_id, results[i][0])
                 results.pop(i)
                 byeDone = True
-            i+=1
+            i += 1
             pass
     """ --------------------"""
-    
     i = 0
-    while len(results)>=1:
-            b=1
+    while len(results) >= 1:
+            b = 1
             withOpponent = False
             while not withOpponent:
-                if not alreadyPlay(tournament_id,results[i][0],results[b][0]): #Prevent rematches
-                    pairs.append((results[i][0],results[i][1],results[b][0],results[b][1]))
+                if not alreadyPlay(tournament_id, results[i][0], results[b][0]):
+                    pairs.append((results[i][0], results[i][1], results[b][0], results[b][1]))
                     results.pop(i)
                     results.pop(b-1)
                     withOpponent = True
-                b+=1
+                b += 1
                 pass
     pass
-
     return pairs
